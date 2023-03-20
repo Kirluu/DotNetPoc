4b1ca2f1d849c42354cb2f225527c171c921008d
Kirluu <kirluu4thewin@gmail.com>
2023-03-20 17:05:07 +0100

    (Barely) Began work on Streaming endpoint POC

---
+++ WebApiSample/Controllers/StreamingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StreamJsonRpc;
+
+namespace WebApiSample.Controllers
+{
+    public class StreamingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/50938522/what-iactionresult-to-return-from-a-websocket-request-in-asp-net-core
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> PostIncomingWebSocketStream()
+        {
+            if (!this.HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                return this.BadRequest();
+            }
+
+            //using (var webSocket = await this.HttpContext.WebSockets.AcceptWebSocketAsync())
+            //{
+            //    using (var jsonRpc = new JsonRpc(new WebSocketMessageHandler(webSocket)))
+            //    {
+            //        jsonRpc.AddLocalRpcTarget(new SocketServer());
+            //        jsonRpc.StartListening();
+            //        await jsonRpc.Completion;
+            //    }
+            //}
+
+            return new EmptyResult();
+        }
+    }
+}
